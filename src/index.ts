443ffba7cdeabc01e76a7bf079063cdbbc302160
sfreeman422 <sfreeman422@protonmail.com>
2020-05-30 11:07:54 -0400

    Updated to provide port specification

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import express, { Application } from 'express';
 import { eventController } from './controller/event.controller';
 
 const app: Application = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(eventController);
 
-app.listen(PORT, (e: Error) => {
+app.listen(+PORT, (e: Error) => {
   e ? console.error(e) : console.log(`Listening on port ${PORT}`);
 });
