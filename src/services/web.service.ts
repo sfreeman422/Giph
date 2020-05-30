d37af3dcbcb125fd9a1d8109bad394e43c059a94
sfreeman422 <sfreeman422@protonmail.com>
2020-05-30 12:14:32 -0400

    Readded context

--- src/services/web.service.ts
+++ src/services/web.service.ts
@@ -116,7 +116,7 @@ export class WebService {
             elements: [
               {
                 type: 'mrkdwn',
-                text: `Posted by: <@${userId}>`,
+                text: `Posted by: <@${userId}> | Search: ${searchTerm}`,
               },
             ],
           },
